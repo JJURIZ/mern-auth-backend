014364ff7f207a5b9b625af3f3d119fc84f410de
Jeremy <jjuriz@gmail.com>
2020-12-17 14:34:59 -0500

    [User] Created User model.

---
+++ models/User.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// User Schema
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        minLength: 8,
+        required: true,
+    },
+    date: {
+        type: Date,
+        default: Date.now()
+    }
+});
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
